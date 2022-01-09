9e61192d52d82d6d5a97e2ab6d7f31b1f8caf018
Gaetz <gaetz@rufflerim.com>
2022-01-09 11:06:45 +0100

    Optimization reflection

--- DumbKnights/components/Move2DSystem.cpp
+++ DumbKnights/components/Move2DSystem.cpp
@@ -9,6 +9,16 @@
 
 void Move2DSystem::update(engine::ecs::Coordinator& coordinator, const GameTime& time) {
 
+    // TODO
+    // Ideally we should get only the components we are interested in (filter by entities) sorted with the same
+    // entity order. This would prevent the use of random access and wwe could use a bare iterator to go through each
+    // component.
+
+    // Because we cannot filter the component array itself, we may want to create a filtered array of pointers
+    // This kind of element should be created when injecting the signature
+
+    // This is the view concept the entt lib uses.
+
     auto transformComps = coordinator.getComponentArray<Transform2D>();
     auto moveComps = coordinator.getComponentArray<Move2D>();
 
